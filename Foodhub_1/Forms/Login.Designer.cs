e256d094da978777d49ab682114fd328111a736c
Kariyawasam Isumjith <isumkariyawasam14@gmail.com>
2026-04-16 11:56:50 +0530

    Initial commit - FoodHub database system

---
+++ Foodhub_1/Forms/Login.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Foodhub_1
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 422);
+            this.Name = "FrmLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Foodhub-Delivery-Management
+Food Hub is a delivery management system made specifically for a database management assignment. the system manages deliveries, branches of the restaurant chain and customer as well s rider and delivery assignments
